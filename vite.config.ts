94fac10b2640710f8ca1b353f0571321f58d4fdb
kazoo-web <katherineullman@gmail.com>
2026-01-25 18:03:35 -0500

    Update vite.config.ts
    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Change 'your-repo-name' to your actual GitHub repository name
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
